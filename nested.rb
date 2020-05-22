895c2ac7b9bcb51c2adcd0d51fc765ed0df5a4f1
Andy Agus <redagus@gmail.com>
2020-05-22 02:43:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,7 +126,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+end
 programmer_hash[:dennis_ritchie][:languages] = programmer_hash[:dennis_ritchie][:languages] += "Assembly"
 return programmer_hash
 end
